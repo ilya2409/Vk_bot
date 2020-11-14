1bb111f4cd9dd8e6fc771a421872ba5e6079541a
ilya2409 <73117875+ilya2409@users.noreply.github.com>
2020-11-14 17:50:28 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -77,7 +77,7 @@ for event in longpoll.listen():
                     
                 elif "Мемчики" in text:
                     write_msg(event.user_id, "https://www.youtube.com/channel/UCJfbG7XfoVkZNakpY0mlXOg")
-                    send_img(event.user_id, "МЕЕЕМ", "photo578623118_457239316", other_keyboard)
+                    send_img(event.user_id, "МЕЕЕМ", "photo-578623118_457239316", other_keyboard)
 
                     
                     
