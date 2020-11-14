4c23081307b5e160715bacae53f793962cb13bce
ilya2409 <73117875+ilya2409@users.noreply.github.com>
2020-11-14 17:09:54 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -68,6 +68,10 @@ for event in longpoll.listen():
                 elif "other" in text:
                     write_msg(event.user_id, """И так, какая 
                                                 тема у нас сегодня?""", other_keyboard)
+                    
+                elif "Музыка" in text:
+                    write_msg(event.user_id, "http://www.loveradio.ru", other_keyboard)
+
 
 
 
