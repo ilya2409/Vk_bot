11ca8ff161ac8becfd0d3783700c41300630fff0
ilya2409 <73117875+ilya2409@users.noreply.github.com>
2021-02-13 17:11:56 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -84,8 +84,8 @@ for event in longpoll.listen():
                                                 тема у нас сегодня?""", other_keyboard)
                     
                 elif "Музыка" in text:
-                    write_msg(event.user_id, "http://www.loveradio.ru",  other_keyboard)
                     write_msg(event.user_id, "https://vk.com/vkmusic",  other_keyboard)
+                    write_msg(event.user_id, "http://www.loveradio.ru",  other_keyboard)
                     
                 elif "ARTS" in text:
                     write_msg(event.user_id, "https://www.deviantart.com/",  other_keyboard)
