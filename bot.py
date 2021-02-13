a10acac947bb361fd3b52cd66dcb8196c5c6b916
ilya2409 <73117875+ilya2409@users.noreply.github.com>
2021-02-13 17:31:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -57,6 +57,9 @@ for event in longpoll.listen():
                         send_img(event.user_id, "Негрусти :Ъ", "photo-191267548_457239052", nani_keyboard)
                        #send_img(event.user_id, "Лови)))", "photo-191267548_457239048", nani_keyboard)
                        #send_img(event.user_id, "Негрусти :Ъ", "photo-191267548_457239046", nani_keyboard)
+                        write_msg(event.user_id, """...или сходи к моему другу,
+                        он поможет""",nani_keyboard)
+                    
                         
                 elif any([(i  in text) for i in ["Дел", "дел"]]):
                     write_msg(event.user_id, """У меня всё Окей, висну сдругими ботами
