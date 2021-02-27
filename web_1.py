7e56ee4e4fb2772a24783c69c560b36582312198
ilya2409 <73117875+ilya2409@users.noreply.github.com>
2021-02-27 22:00:35 +0400

    Update web_1.py

--- web_1.py
+++ web_1.py
@@ -1,3 +1,3 @@
-funk_1 = import webbrowser
-
-funk_2 = webbrowser.open_new('https://www.google.com/search?q=cfqns&oq=cfqns&aqs=chrome..69i57.3053j0j1&sourceid=chrome&ie=UTF-8')
+def web_2():
+  import webbrowser
+  webbrowser.open_new('https://news.google.com')
